c780f7b0a5e37c92dd13307648d7f261e234abb2
IB2M_Official <ib2m.morel@gmail.com>
2025-04-06 19:40:20 +0200

    Update disbox-server.js

--- disbox-server.js
+++ disbox-server.js
@@ -6,7 +6,10 @@ var bodyParser = require('body-parser')
 const disboxFile = require('./disbox-file');
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
 
 app.use(cors())
 app.use(bodyParser.json())
